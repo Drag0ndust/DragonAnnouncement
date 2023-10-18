4acf7a72188f1858ac632e22fbf03e1436c19475
Thimo Bess <thimo.bess@gmail.com>
2023-10-18 08:54:57 +0200

    Feature/2 example app (#6)
    * Add empty Xcode project
    * Add swift package to the project

---
+++ ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Drag0ndust on 18.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
